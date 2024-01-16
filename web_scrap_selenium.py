a9d76d9425b13de6d1c5655d4f03890c8608c95b
luciano jr <lucianojr0201@gmail.com>
2024-01-16 15:04:08 -0300

    atualizar

--- web_scrap_selenium.py
+++ web_scrap_selenium.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 #from selenium.webdriver.edge.options import Options
 
 #options = Options()
@@ -17,16 +19,14 @@ navegador.get('https://www.catho.com.br/vagas/')
 sleep(3)
 vaga = navegador.find_element(By.ID,'keyword')
 
-vaga.send_keys('cientista de dados')
+vaga.send_keys('analista')
+vaga.send_keys(Keys.RETURN)
 
 estado = navegador.find_element(By.ID,'location')
+
+estado.clear()
+estado.send_keys("rio de janeiro")
 sleep(2)
-
-
-estado.send_keys('Rio de Janeiro')
-sleep(2)
-primeira_sugestao = estado.find_element(By.TAG_NAME, "Rio de Janeiro/RJ")
-
-
+estado.send_keys(Keys.RETURN)
 
 sleep(1000)
